b6fc38240a4d38beac573df38fefbe9ad80ec6d6
zgljl2012 <2693491512@qq.com>
2023-09-21 09:52:28 +0800

    integrate tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./styles.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <h1 className="text-red-800">Hello</h1>
     <App />
   </React.StrictMode>,
 );
